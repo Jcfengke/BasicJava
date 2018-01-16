25ad08b6a10902219843868d22c3ae60e85fb63d
Bill <jinfeng.song@vip.163.com>
2018-01-16 20:05:15 +0800

    添加volatile字段的内容

--- src/com/feng/thread/BasicSynchronization.java
+++ src/com/feng/thread/BasicSynchronization.java
@@ -9,6 +9,9 @@ package com.feng.thread;
  *      互斥性
  *      可见行
  *
+ * Java 语言包含两种内在的同步机制：同步块（或方法）和 volatile 变量。这两种机制的提出都是为了实现代码线程的安全性。
+ * 其中 Volatile 变量的同步性较差（但有时它更简单并且开销更低），而且其使用也更容易出错。
+ *
  * 概念：
  *      竞态条件：
  *              check-then-act
@@ -190,12 +193,52 @@ public class BasicSynchronization {
      * =================================================================================================================
      * volatile和final
      *
+     *Volatile 变量具有 synchronized 的可见性特性，但是不具备原子特性。这就是说线程能够自动发现 volatile 变量的最新值。
+     *下载Volatile 变量可用于提供线程安全，但是只能应用于非常有限的一组用例：多个变量之间或者某个变量的当前值与修改后值之间没有约束。
+     *因此，单独使用 volatile 还不足以实现计数器、互斥锁或任何具有与多个变量相关的不变式（Invariants）的类（例如 “start <=end”）。
+     *
+     * 要使 volatile 变量提供理想的线程安全，必须同时满足下面两个条件：
+     *                  对变量的写操作不依赖于当前值。
+     *                          第一个条件的限制使 volatile 变量不能用作线程安全计数器。虽然增量操作（x++）看上去类似一个单独操作，实际上它是一个由读取－修改－写入操作序列组成的组合操作，必须以原子方式执行，而 volatile 不能提供必须的原子特性。实现正确的操作需要使 x 的值在操作期间保持不变，而 volatile 变量无法实现这点。（然而，如果将值调整为只从单个线程写入，那么可以忽略第一个条件。）
+     *                  该变量没有包含在具有其他变量的不变式中。
      *
      */
+
+
+
 
     public static void main(String[] args) {
         BasicSynchronization basicSynchronization=new BasicSynchronization();
         //basicSynchronization.cacheVariables();
-        new DeadlockDemo().run();
+        //new DeadlockDemo().run();
+        new StopThread().execut();
     }
+}
+class StopThread{
+    //
+    private volatile   boolean flag=false;
+    //多试几次  可以看到循环的打印数据
+    //private  boolean flag=false;
+    private  int counter=0;
+
+    public void  execut(){
+
+        Runnable r=new Runnable() {
+            @Override
+            public void run() {
+                while (!flag){
+                    for (int i=0;i<5;i++)
+                        System.out.println("this is test:"+counter++);
+                }
+            }
+        };
+
+        Thread thread=new Thread(r);
+        thread.setPriority(Thread.MAX_PRIORITY);
+        thread.start();
+
+        flag=true;
+    }
+
+
 }
